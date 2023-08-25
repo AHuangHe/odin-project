3322baa70fca95153a8e69c3ff8f225f07e66b4a
Alberto Huang He <aahuanghe@gmail.com>
2023-08-24 20:01:47 -0700

    ah - Add javascript variables lesson

---
+++ javascript/js-funds.js
@@ -0,0 +1,43 @@
+//let message = 'Hello'; // store string 'Hello' in the variable message
+//alert(message);
+
+/*
+We can declare multiple variables in one line
+let user = 'John', age = 25, message = 'Hello';
+
+But for the sake of readability, do them line by line
+let user = 'John';
+let age = 25;
+let message = 'Hello';
+
+or
+
+let user = 'John',
+  age = 25,
+  message = 'Hello';
+
+or
+
+let user = 'John'
+  , age = 25
+  , message = 'Hello';
+
+we can declare two variables and copy data from one into the other
+
+let hello = 'Hello world!';
+let message;
+
+message = hello;
+
+declaring
+*/
+
+// Variables Tasks
+let admin;
+let name = 'John';
+
+admin = name;
+alert(admin)
+
+let ourPlanetName = 'Earth';
+let currentUserName = 'John';
